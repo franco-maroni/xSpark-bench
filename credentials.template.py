4a13f7fb5be7dfbfc3f6cd188ec1900c5581cec2
Francesco Marconi <franco.maroni@gmail.com>
2017-11-23 10:19:23 +0000

    add key info to credentials.template

--- credentials.template.py
+++ credentials.template.py
@@ -16,3 +16,11 @@ AWS_ACCESS_ID = ""
 """AWS Access ID"""
 AWS_SECRET_KEY = ""
 """AWS Secret"""
+
+# ssh-keygen -t rsa -b 2048
+AZ_KEY_NAME = "az_id_rsa"
+"""Name of the RSA 2048 key"""
+AZ_PUB_KEY_PATH = '/home/ubuntu/.ssh/' + AZ_KEY_NAME + '.pub'
+"""AZURE Public Key Path (RSA 2048)"""
+AZ_PRV_KEY_PATH = '/home/ubuntu/.ssh/' + AZ_KEY_NAME
+"""AZURE Private Key Path (RSA 2048)"""
